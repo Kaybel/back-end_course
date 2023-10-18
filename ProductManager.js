bf033d51d1d411e154e97a508f55fa0351a92f73
alejandra <kblmrtnz@gmail.com>
2023-10-18 00:48:54 -0300

    add testing

--- ProductManager.js
+++ ProductManager.js
@@ -58,7 +58,7 @@ class ProductManager {
         }
     }
 
-    async updateProductById(id, product) {
+    async updateProduct(id, product) {
         try {
             const products = await this.getProducts()
             const productIndex = products.findIndex((product) => product.id === id)
@@ -69,13 +69,14 @@ class ProductManager {
                 products[productIndex] = product
                 // overwrite the json with the updated data
                 await fs.promises.writeFile(this.path, JSON.stringify(products))
+                return 'Product updated successfully'
             } else return 'Product not found'
         } catch (error) {
             console.log(error)
         }
     }
 
-    async deleteProductById(id) {
+    async deleteProduct(id) {
         try {
             const products = await this.getProducts()
             const productIndex = products.findIndex((product) => product.id === id)
@@ -90,4 +91,72 @@ class ProductManager {
             console.log(error)
         }
     }
+
+    async testing() {
+        // Objetos de prueba
+        const product1 = {
+            title: 'product test',
+            description: 'Este es un producto prueba',
+            price: 200,
+            thumbnail: 'Sin imagen',
+            code: 'abc123',
+            stock: 25
+        }
+
+        const product2 = {
+            title: 'product test',
+            description: 'Este es un producto prueba',
+            price: 200,
+            thumbnail: 'Sin imagen',
+            code: 'abc456',
+            stock: 25
+        }
+
+        const product3 = {
+            title: 'product test',
+            description: 'Este es un producto prueba',
+            price: 200,
+            thumbnail: 'Sin imagen',
+            code: 'abc789',
+            stock: 25
+        }
+
+        // Se creará una instancia de la clase “ProductManager”
+        const productManager = new ProductManager()
+
+        // Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []
+        console.log(await productManager.getProducts())
+
+        // Se llamará al método “addProduct” con los campos:
+        // title: “producto prueba”
+        // description:”Este es un producto prueba”
+        // price:200,
+        // thumbnail:”Sin imagen”
+        // code:”abc123”,
+        // stock:25
+        // El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
+        console.log(await productManager.addProduct(product1))
+        console.log(await productManager.addProduct(product2))
+        console.log(await productManager.addProduct(product3))
+
+        //Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado
+        console.log(await productManager.getProducts())
+
+        //Se llamará al método “getProductById” y se corroborará que devuelva el producto con el id especificado, en caso de no existir, debe arrojar un error.
+        console.log(await productManager.getProductById(1))
+        console.log(await productManager.getProductById(15))
+
+        //Se llamará al método “updateProduct” y se intentará cambiar un campo de algún producto, se evaluará que no se elimine el id y que sí se haya hecho la actualización.
+        console.log(await productManager.updateProduct(1, product3))
+        console.log(await productManager.getProducts())
+
+        //Se llamará al método “deleteProduct”, se evaluará que realmente se elimine el producto o que arroje un error en caso de no existir.
+        console.log(await productManager.deleteProduct(1))
+        console.log(await productManager.getProducts())
+    }
 }
+
+//fs.unlink('./products.json')
+  
+const test = new ProductManager();
+test.testing();
